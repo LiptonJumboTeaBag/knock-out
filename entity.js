768f51bed79ab0039c3820fc4535ee62467c2c89
John Li <johnjohnli2015@hotmail.com>
2022-11-05 01:21:59 -0700

    Moved material and shape config to entities

--- entity.js
+++ entity.js
@@ -4,7 +4,22 @@ const {
     Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene,
 } = tiny;
 
+const shapes = {
+    rectangle: new defs.Cube(),
+};
 
+const phong = new defs.Phong_Shader();
+
+const materials = {
+
+    plastic: new Material(phong,
+        {ambient: .2, diffusivity: .8, specularity: .5, color: color(.9, .5, .9, 1)}),
+    metal: new Material(phong,
+        {ambient: 1, diffusivity: .8, specularity: .8, color: color(.9, .5, .9, 1)}),
+    table: new Material(phong,
+        {ambient: 1, diffusivity: 0, specularity: 0, color: color(0.2, .5, 1, 1)})
+
+};
 /**
  * Entity is the base class for all 3D objects in the game.
  */
@@ -50,16 +65,18 @@ export class Ball extends Entity {
 }
 
 export class Table extends Entity {
-    // place table at origin with scale_x, scale_y, scale_z with the specified materal
-    constructor(material, scale_x= 2.5, scale_y = 0.5, scale_z=5){
+    // place table at origin with scale_x, scale_y, scale_z with the specified materal\
+    // the default shape is a cube
+    constructor(material = materials.table, shape = shapes.rectangle, scale_x = 2.5, scale_y = 0.5, scale_z = 5){
         super();
         this.position = this.position
             .times(Mat4.scale(scale_x,scale_y,scale_z));
         this.material = material;
+        this.shape = shape;
     }
 
     draw(context, program_state){
-        new defs.Cube().draw(context, program_state, this.position, this.material);
+        this.shape.draw(context, program_state, this.position, this.material);
     }
 }
 
--- knock-out.js
+++ knock-out.js
@@ -18,20 +18,7 @@ export class KnockOut extends Scene {
         this.colliders = [];
         this.cameras = [];
         this.ui = [];
-        this.game = null;
-        this.camera = new Camera()
-           
-        const phong = new defs.Phong_Shader();
-        this.materials = {
-
-            plastic: new Material(phong,
-                {ambient: .2, diffusivity: .8, specularity: .5, color: color(.9, .5, .9, 1)}),
-            metal: new Material(phong,
-                {ambient: 1, diffusivity: .8, specularity: .8, color: color(.9, .5, .9, 1)}),
-            table: new Material(phong,
-                {ambient: 1, diffusivity: 0, specularity: 0, color: color(0.2, .5, 1, 1)})
-      
-            };
+        this.game = null;        
         
     }
 
@@ -43,8 +30,9 @@ export class KnockOut extends Scene {
      */
     display(context, program_state) {
 
+        this.cameras = [new Camera()]
         program_state.set_camera(
-           this.camera.birdEye(10, vec3(1,0,0)));  
+           this.cameras[0].birdEye(10, vec3(1,0,0)));  
 
         program_state.projection_transform = Mat4.perspective(
             Math.PI / 4, context.width / context.height, 1, 100);
@@ -52,7 +40,7 @@ export class KnockOut extends Scene {
         const light_position = vec4(0,0,0,1);
         program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 1000)];
         
-        this.entities = {table: new Table(this.materials.table)};
+        this.entities = {table: new Table()};
         this.entities.table.draw(context, program_state);
     }
 }
