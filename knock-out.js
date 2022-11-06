073fa71fe0bb82198bc7aa45dbb0bbd16bc289d0
Xuyang Zhou <nick.zhouxy@gmail.com>
2022-11-06 15:42:19 -0800

    Basic UI rendering routine
    Setup basic UI rendering routine in the main scene.

--- knock-out.js
+++ knock-out.js
@@ -1,6 +1,7 @@
 import {defs, tiny} from './tiny-graphics/common.js';
 import {Camera} from "./camera.js";
 import {Obstacle, Table} from "./entity.js";
+import {Scoreboard, UI} from "./ui.js";
 
 const {
     Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene,
@@ -14,44 +15,65 @@ export class KnockOut extends Scene {
     constructor() {
         super();
 
-        this.entities = {};
         this.colliders = [];
         this.cameras = [];
-        this.ui = [];
-        this.game = null;        
+        this.ui = [new Scoreboard()];
+        this.game = null;
         this.cameras = [new Camera()]
         this.entities = {
             table: new Table(),
             obstacle1: new Obstacle(),
         };
 
+        this.frame_rate = 0;
     }
 
     make_control_panel() {
+        this.live_string(box => {
+            box.textContent = `Frame rate: ${this.frame_rate.toFixed(2)}`;
+        });
     }
 
     /**
      * Handles the display and update of all objects.
      */
     display(context, program_state) {
-
+        // Setup control panel
         if (!context.scratchpad.controls) {
             this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
 
             program_state.set_camera(
-            //this.cameras[0].birdEye(10, vec3(1,0,0)));  
-            //this.cameras[0].LeftPerspective());
-            this.cameras[0].RightPerspective());
+                //this.cameras[0].birdEye(10, vec3(1,0,0)));
+                //this.cameras[0].LeftPerspective());
+                this.cameras[0].RightPerspective()
+            );
         }
+
+        // Calculate time
+        const t = program_state.animation_time / 1000, dt = program_state.animation_delta_time / 1000;
+
+        // Update frame rate
+        this.frame_rate = 1 / dt;
+
+        // Setup projection matrix
         program_state.projection_transform = Mat4.perspective(
             Math.PI / 4, context.width / context.height, 1, 100);
-        
-        const light_position = vec4(0,0,0,1);
+
+        // Setup light
+        const light_position = vec4(0, 0, 0, 1);
         program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 1000)];
-        
-        for (var i in this.entities){
-            console.log(i)
+
+        // Update and draw all entities
+        for (const i in this.entities) {
+            // console.log(i)
             this.entities[i].draw(context, program_state);
+        }
+
+        // Update and draw all ui
+        UI.update_camera(program_state.camera_inverse);  // Only need to update camera once
+        // console.log(program_state.camera_inverse.toString())
+        for (const i in this.ui) {
+            this.ui[i].display(context, program_state);
         }
     }
 }
