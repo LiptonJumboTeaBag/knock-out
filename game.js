4da693f6c56e42e03864ae8f62817e14e304a4a5
Ryan Yeo <ryandc.yeo@gmail.com>
2022-12-01 15:57:52 -0800

    game logic

--- game.js
+++ game.js
@@ -10,5 +10,58 @@ const {
  */
 export class Game {
     constructor() {
+        // super();
+        // keeping track of which player's turn it is, player1 goes first
+        this.player1_turn = true;
+        this.player2_turn = false;
+        // keeping track of current round
+        this.curr_round = 1;
+
+        // starting number of chips
+        this.numChips1 = 3; // we can decide how many chips to start with
+        this.numChips2 = 3;
+
+
     }
+
+    // USAGE: use this function twice in each round
+    //          first usage triggers player1's turn
+    //          second usage triggers player2's turn and immediately plays animation once player2's turn is over
+    // just kidding animation plays once m is pressed
+    play() {
+        if (this.player1_turn) {
+            // player1's turn and change perspective
+            console.log('p1');
+        } else if (this.player2_turn) {
+            // player2's turn and change perspective
+            // game animation
+            console.log('p2');
+        }
+
+        this.isPlayer1 = !this.isPlayer1;
+    }
+
+    // USAGE: this function should be used once after the total number of chips knocked out on both sides is known when the current round ends
+    //          it allows for us to consider to possibility of a tie
+    knockout(p1, p2) {
+        if (p1 === 0 && p2 === 0) {
+            return 2;   // can decide on the output later
+        } else if (p1 === 0) {
+            return 1;
+        } else if (p2 === 0) {
+            return 0;
+        } else {
+            return "next round";
+        }
+    }
+
+    //  GAME LOGIC, play turns:
+    //      player1 chooses the direction and strength for each available chip1
+    //      player2 chooses the direction and strength for each available chip2
+    //      game animation
+    //      update on remaining chips for each side
+
+    //  GAME LOGIC, play until:
+    //      no chips for one player ==> other player wins
+    //      no chips for both players ==> draw
 }
